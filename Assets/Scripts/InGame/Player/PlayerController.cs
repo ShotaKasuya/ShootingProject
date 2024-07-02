9e2bf031fa5d56c1abbc14fffe7deb174c522379
Wans1ba <syusuke.okada0703@icloud.com>
2024-07-02 19:53:19 +0900

    おい
    こっちみんな殺すぞ

--- Assets/Scripts/InGame/Player/PlayerController.cs
+++ Assets/Scripts/InGame/Player/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        printf("岡田だと思うなよ\n");
     }
 
     // Update is called once per frame
