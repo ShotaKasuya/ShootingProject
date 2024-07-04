6fede5fb6081c225f829e27cbf751afbd337f773
Dorameta <130573159+Dorameta@users.noreply.github.com>
2024-07-04 19:10:57 +0900

    test

--- Assets/Scripts/InGame/Player/PlayerController.cs
+++ Assets/Scripts/InGame/Player/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        title
     }
 
     // Update is called once per frame
